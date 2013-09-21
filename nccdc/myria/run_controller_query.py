07838391e7a9b55ff564d8a19105b126b739f0b7
Andrew Whitaker <whitaker@cs.washington.edu>
2013-09-21 08:33:19 -0700

    Fix ports in query execution scripts

--- nccdc/myria/run_controller_query.py
+++ nccdc/myria/run_controller_query.py
@@ -7,5 +7,5 @@ import controller_query
 
 plan = controller_query.generate()
 
-subprocess.check_call(['curl', '-i', '-XPOST', 'localhost:8753/query', '-H',
+subprocess.check_call(['curl', '-i', '-XPOST', 'localhost:1776/query', '-H',
                        'Content-type: application/json',  '-d',  plan])
--- nccdc/myria/run_victim_query.py
+++ nccdc/myria/run_victim_query.py
@@ -7,5 +7,5 @@ import victim_query
 
 plan = victim_query.generate()
 
-subprocess.check_call(['curl', '-i', '-XPOST', 'localhost:8753/query', '-H',
+subprocess.check_call(['curl', '-i', '-XPOST', 'localhost:1776/query', '-H',
                        'Content-type: application/json',  '-d',  plan])
