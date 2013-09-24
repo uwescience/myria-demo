13f3a159849ed3437a2e97ee56c232e3011065c6
Andrew Whitaker <whitaker@cs.washington.edu>
2013-09-24 00:34:04 -0700

    ingest 100m sp2bench triples

---
+++ sp2bench/ingest_100m.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import subprocess
+
+json = """{
+  "relation_key" : {
+    "user_name" : "public",
+    "program_name" : "adhoc",
+    "relation_name" : "sp2bench_100m"
+  },
+  "schema" : {
+    "column_types" : ["STRING_TYPE", "STRING_TYPE", "STRING_TYPE"],
+    "column_names" : ["subject", "predicate", "object"]
+  },
+  "file_name" : "file:///disk1/whitaker/sp2bench/sp2b/bin/sp2b_100m.txt"
+}"""
+
+subprocess.check_call(['curl', '-i', '-XPOST',
+                       'vega.cs.washington.edu:1776/dataset', '-H',
+                       'Content-type: application/json',  '-d',  json])
