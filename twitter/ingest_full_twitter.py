2c375a8d9ab57491dce8e83162bcc08d47d08cfc
Daniel Halperin <dhalperi@cs.washington.edu>
2013-09-30 09:44:23 -0700

    add full twitter to myria
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

---
+++ twitter/ingest_full_twitter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import subprocess
+
+json = """{
+  "relation_key" : {
+    "user_name" : "public",
+    "program_name" : "adhoc",
+    "relation_name" : "Twitter"
+  },
+  "schema" : {
+    "column_types" : ["INT_TYPE", "INT_TYPE"],
+    "column_names" : ["followee", "follower"]
+  },
+  "file_name" : "hdfs://vega.cs.washington.edu:8020//datasets/twitter"
+}
+"""
+
+subprocess.check_call(['curl', '-i', '-XPOST', 'vega.cs.washington.edu:1776/dataset', '-H',
+ 'Content-type: application/json',  '-d',  json])
